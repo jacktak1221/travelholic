9c07c1776f5018c7a909e142d08d44014c536c4d
Jack <jackapple04@gmail.com>
2018-12-02 22:46:14 +0800

    Added Material UI

---
+++ travelholic/app/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/App.css';
+import Main from './Main';
+
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <div className="container">
+                <Main/>
+            </div>
+        );
+    }
+}
+
+export default App;
